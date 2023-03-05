243a8f81a778b59abd72dbba6cb83ce42bbcb4ce
sunil dev <bindusunil58@gmail.com>
2023-03-05 13:16:58 +0530

    initial commit

---
+++ src/main/java/com/practise/petClinic/SunilPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.practise.petClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunilPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunilPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practise/petClinic/SunilPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.petClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunilPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
